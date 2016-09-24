39ee483540757239a401452bf6b599985a795596
penguinMary <pingvin.mary@gmail.com>
2016-09-24 16:09:31 +0200

    Продолжение дружбы с git

--- main.cpp
+++ main.cpp
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-#include "erosmain.h"
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    ErosMain w;
-    w.show();
-
-    return a.exec();
-}
-=======
 #include "erosmain.h"
 #include <QApplication>
 
